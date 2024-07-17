971b3b561ce2de43e3719fd93ce38ec26a7f50b4
zhangke <zhangke@zdns.cn>
2024-07-17 13:44:25 +0800

    Perfect

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/godaner/clonefile
 
-go 1.17
+go 1.20
+
+require (
+	github.com/duke-git/lancet/v2 v2.3.1
+	github.com/samber/lo v1.46.0
+)
+
+require golang.org/x/text v0.16.0 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/duke-git/lancet/v2/fileutil"
+	"github.com/samber/lo"
 	"io/fs"
 	"io/ioutil"
 	"os"
@@ -12,7 +14,11 @@ import (
 	"time"
 )
 
-var src, dst, exclude, prefix string
+const (
+	timeFormat = "2006_01_02_15_04_05"
+)
+
+var src, dst, srcLastDir, exclude, prefix string
 var interval, max int64
 var excludeM map[string]bool
 
@@ -26,107 +32,129 @@ func init() {
 	flag.Parse()
 }
 func main() {
+	checkFlag()
+	initParam()
+	go loopClone()
+	go loopDelete()
+	select {}
+}
+
+func checkFlag() {
 	if src == "" || dst == "" || prefix == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
-	excludeM = map[string]bool{}
-	excludes := strings.Split(exclude, ",")
-	for _, exc := range excludes {
-		excludeM[exc] = true
-	}
-	sn, err := filepath.Abs(src)
+}
+
+func initParam() {
+	excludeM = lo.SliceToMap(strings.Split(exclude, ","), func(s string) (string, bool) {
+		return s, true
+	})
+	var err error
+	src, err = filepath.Abs(src)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Get src abs path err:", err)
 		os.Exit(1)
 	}
 	// /Users/godaner/gomod/clonefile/bin/darwin-arm64
-	fmt.Println("src abs path", sn)
-	snSplit := strings.Split(sn, string(filepath.Separator))
-	sName := snSplit[len(snSplit)-1]
-	// darwin-arm64
-	fmt.Println("src abs dir", sName)
-	dn, err := filepath.Abs(dst)
-	if err != nil {
-		fmt.Println(err)
+	fmt.Println("Src abs path:", src)
+	if !fileutil.IsDir(src) {
+		fmt.Println("Src abs path is not dir")
 		os.Exit(1)
 	}
-	// /Users/godaner/gomod/clonefile/bin/
-	fmt.Println("dst abs path", dn)
-	go func() {
-		for {
-			go func() {
-				defer func() {
-					if err := recover(); err != nil {
-						fmt.Println("recover clone file err:", err)
-					}
-				}()
-				err = clonefile(sn, dn, sName)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-			}()
-			<-time.After(time.Duration(interval) * time.Second)
-		}
-	}()
-	go func() {
-		for {
-			go func() {
-				defer func() {
-					if err := recover(); err != nil {
-						fmt.Println("recover delete err:", err)
-					}
-				}()
-				dirs := sort.StringSlice{}
-				filepath.Dir(dst)
-				err = filepath.WalkDir(dst, func(path string, d fs.DirEntry, err error) error {
-					if !strings.Contains(path, prefix) {
-						return nil
-					}
-					ps := strings.Split(path, string(filepath.Separator))
-					if len(ps) <= 0 {
-						return nil
-					}
-					if !strings.Contains(ps[len(ps)-1], prefix) {
-						return nil
-					}
-					dirs = append(dirs, path)
-					return nil
-				})
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				dirs.Sort()
-				m := dirs.Len() - int(max)
-				if m <= 0 {
-					return
-				}
-				delDirs := (([]string)(dirs))[:m]
-				for _, dd := range delDirs {
-					fmt.Println("Delete dir", dd)
-					err = os.RemoveAll(dd)
-					if err != nil {
-						fmt.Println(err)
-					}
-				}
-			}()
-			<-time.After(time.Duration(1) * time.Second)
-		}
-	}()
-	select {}
+	// darwin-arm64
+	srcLastDir = filepath.Base(src)
+	fmt.Println("Src abs last dir:", srcLastDir)
+
+	dst, err = filepath.Abs(dst)
+	if err != nil {
+		fmt.Println("Get dst abs path err:", err)
+		os.Exit(1)
+	}
+	// /Users/godaner/gomod/clonefile/bin
+	fmt.Println("Dst abs path:", dst)
+
+	if !fileutil.IsDir(dst) {
+		fmt.Println("Dst abs path is not dir")
+		os.Exit(1)
+	}
 }
 
-func clonefile(src string, dst string, name string) error {
-	fmt.Println("Clone...")
+func loopDelete() {
+	for {
+		func() {
+			defer func() {
+				if err := recover(); err != nil {
+					fmt.Println("Recover delete err:", err)
+				}
+			}()
+			dirs := sort.StringSlice{}
+			filepath.Dir(dst)
+			err := filepath.WalkDir(dst, func(path string, d fs.DirEntry, err error) error {
+				if !strings.Contains(path, prefix) {
+					return nil
+				}
+				ps := strings.Split(path, string(filepath.Separator))
+				if len(ps) <= 0 {
+					return nil
+				}
+				if !strings.Contains(ps[len(ps)-1], prefix) {
+					return nil
+				}
+				dirs = append(dirs, path)
+				return nil
+			})
+			if err != nil {
+				fmt.Printf("Walk dir: %v err: %v\n", dst, err)
+				return
+			}
+			dirs.Sort()
+			m := dirs.Len() - int(max)
+			if m <= 0 {
+				return
+			}
+			delDirs := (([]string)(dirs))[:m]
+			for _, dd := range delDirs {
+				fmt.Println("Deleting dir:", dd)
+				err = os.RemoveAll(dd)
+				if err != nil {
+					fmt.Printf("Remove dir: %v err: %v\n", dd, err)
+					continue
+				}
+				fmt.Printf("Deleting dir: %v success\n", dd)
+			}
+		}()
+		<-time.After(time.Duration(1) * time.Second)
+	}
+}
+
+func loopClone() {
+	for {
+		func() {
+			defer func() {
+				if err := recover(); err != nil {
+					fmt.Println("Recover clone file err:", err)
+				}
+			}()
+			err := clonefile()
+			if err != nil {
+				fmt.Println("Clone file err:", err)
+				return
+			}
+		}()
+		<-time.After(time.Duration(interval) * time.Second)
+	}
+}
+
+func clonefile() error {
+	fmt.Println("Cloning...")
 	defer func() {
 		fmt.Println("Finish!")
 	}()
-	ts := time.Now().Format("2006_01_02_15_04_05")
+	ts := time.Now().Format(timeFormat)
 	err := filepath.WalkDir(src, func(path string, d fs.DirEntry, err error) error {
 		if d.IsDir() {
-			nName := strings.ReplaceAll(path, src, dst+string(filepath.Separator)+prefix+"_"+ts+"_"+name)
+			nName := strings.ReplaceAll(path, src, dst+string(filepath.Separator)+prefix+"_"+ts+"_"+srcLastDir)
 			err = os.Mkdir(nName, 0777)
 			if err != nil {
 				return err
@@ -134,14 +162,14 @@ func clonefile(src string, dst string, name string) error {
 			return nil
 		}
 		if excludeM[d.Name()] {
-			fmt.Println("Ignore file", d.Name())
+			fmt.Println("Ignore file:", d.Name())
 			return nil
 		}
 		bs, err := ioutil.ReadFile(path)
 		if err != nil {
 			return err
 		}
-		nName := strings.ReplaceAll(path, src, dst+string(filepath.Separator)+prefix+"_"+ts+"_"+name)
+		nName := strings.ReplaceAll(path, src, dst+string(filepath.Separator)+prefix+"_"+ts+"_"+srcLastDir)
 		fmt.Println("Clone", path, "to", nName)
 		err = ioutil.WriteFile(nName, bs, 0777)
 		if err != nil {
